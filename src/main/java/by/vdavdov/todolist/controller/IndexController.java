d76f36ff2358984f59a85fc48ec17f4d5ed49dcd
vdavdov <sevadavydov.06z@mail.ru>
2024-08-05 18:37:57 +0400

    Add i18n

--- src/main/java/by/vdavdov/todolist/controller/IndexController.java
+++ src/main/java/by/vdavdov/todolist/controller/IndexController.java
@@ -8,26 +8,50 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @Controller
 @RequestMapping(value = {"/index", "/tasks", "/"})
 @RequiredArgsConstructor
 public class IndexController {
+
     private final TaskService taskService;
 
     @GetMapping
-    public String get(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size, Model model) {
+    public String get(@RequestParam(defaultValue = "0") int page,
+                      @RequestParam(defaultValue = "5") int size,
+                      Model model) {
         Pageable pageable = PageRequest.of(page, size);
         List<TaskTo> tasks = taskService.getAll();
         model.addAttribute("tasks", tasks);
-        model.addAttribute("page", page);
+        model.addAttribute("currentPage", page);
         model.addAttribute("size", size);
+        model.addAttribute("newTask", new Task());
+        model.addAttribute("totalPages", taskService.findPaginated(page + 1, size).getTotalPages());
+        return "index";
+    }
+
+    @PostMapping
+    public String post(@ModelAttribute TaskTo task, Model model) {
+        taskService.create(task);
+        return getString(model);
+    }
+
+    @PostMapping(value = "/delete/{id}")
+    public String delete(@PathVariable Long id, Model model) {
+        taskService.deleteById(id);
+        return getString(model);
+    }
+
+    private String getString(Model model) {
+        List<TaskTo> tasks = taskService.getAll();
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("currentPage", 0);
+        model.addAttribute("size", 5);
+        model.addAttribute("newTask", new Task());
+        model.addAttribute("totalPages", taskService.findPaginated(1, 5).getTotalPages());
         return "index";
     }
 }
--- src/main/java/by/vdavdov/todolist/service/TaskService.java
+++ src/main/java/by/vdavdov/todolist/service/TaskService.java
@@ -5,6 +5,8 @@ import by.vdavdov.todolist.model.dto.TaskTo;
 import by.vdavdov.todolist.model.entity.Task;
 import by.vdavdov.todolist.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +23,11 @@ public class TaskService {
     public Task create(TaskTo taskTo) {
         Task entity = taskDtoMapper.toEntity(taskTo);
         return taskRepository.save(entity);
+    }
+
+    public Page<Task> findPaginated(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return taskRepository.findAll(pageable);
     }
 
     public TaskTo getById(Long id) {
